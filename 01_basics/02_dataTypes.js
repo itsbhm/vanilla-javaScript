6dea16096b163914e0e9abeebd8012ced6993464
Shubham <shubham.17@hotmail.com>
2024-11-09 22:57:05 +0530

    Resource URLs Added

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,4 +3,8 @@
 // alert("Hi")
 
 console.log("Hi")
-console.log("I am JS")
+console.log("I am JS") // Code Readability Should be High
+
+// ECMA Script Draft URL: https://tc39.es/ecma262/
+// MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
